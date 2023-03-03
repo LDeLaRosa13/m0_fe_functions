c4efb319f59838632edc34570f82b3337d60f691
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-03-02 19:44:13 -0500

    show updates for each file

--- builtInMethods.js
+++ builtInMethods.js
@@ -8,13 +8,19 @@
 // The return value is "hello world"
 "Hello World".toLowerCase();
 
-
+// The includes() method is called on the string "Hello World"
+// Argument includes "Hello"
+// The return value is true
 "Hello World".includes("Hello");
 
-
+// The endsWith() method is called on the string "Hello World"
+// Argument includes "Hello" 
+// The return value is false
 "Hello World".endsWith("Hello");
 
-
+// The endWith() method is called on the string "Hello World"
+// argument includes "rld"
+// The return value is true
 "Hello World".endsWith("rld");
 
 
@@ -24,6 +30,13 @@
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 // Include comments above each method call explaining the impact and return value of that method.
+var dog = "cute"
+var cat = "adorable"
+// .startsWith method will determine if variables assigned to strings are true or false. 
+// Return value will be either true or false.
+console.log(dog.startsWith ("c"));
+// .toUpperCase method will make the string entirely uppercase.
+console.log(cat.toUpperCase());
 
 // EXAMPLE
 // The startsWith() method is called on the firstName variable, which stores the string object "Jeff". 
@@ -40,3 +53,14 @@ console.log(firstName.startsWith("J"));
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Include comments above each method call explaining the impact and return value of that method.
+var shoeSizes = [6, 8, 9, 11, 14];
+// .pop() method removes the last element and returns the value without it
+    shoeSizes.pop()
+    console.log(shoeSizes)
+
+var friends = ["Chase", "Anthony", "Stephen", "Jen", "Polly"]
+// .shift() method removes the element in index position [0].
+// return value is made up of the remaining index positions.
+    friends.shift(0)
+    console.log(friends)    
+    
--- functions.js
+++ functions.js
@@ -3,21 +3,53 @@
 // Call each method at least twice and store the return value in a variable. 
 // Use console.log() to see the return value in the console.
 
-// 1: Write a function named greeting that returns a string with a general greeting. 
+// // 1: Write a function named greeting that returns a string with a general greeting. 
+function greeting() {
+    return "Welcome in!"
+}
+var welcome = greeting()
+console.log(welcome);
 
-
-// 2: Write a function named customGreeting that returns a greeting WITH a specific name.
-
-
+// // // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+function customGreeting(name) {
+    return "Hey there Anthony" 
+}
+var greet = customGreeting()
+console.log(greet)
+customGreeting();
+    
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+function greetPerson(firstName, middleName, lastName) {
+    return "hi there lauren ann delarosa!"
+}
+var names = greetPerson()
+console.log(names)
 
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
+function square(number) {
+    return number * number
+}
+    var shape = square 
+    console.log(square(28))
 
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
+
+function checkStock(amount, item) {
+    if (amount >= 4) {
+    console.log(`${item} is stocked`);    
+}   else if (amount >= 1) {
+    console.log(`${item} is running low`); 
+}   else  {
+    console.log(`${item} is out of stock`);
+
+}
+}   
+
+
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
